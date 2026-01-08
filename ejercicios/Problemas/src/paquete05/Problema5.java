bf7191833337a591f8f542dd10e6908bfcc66222
GIT-gezzy <jdavichomaldonado@gmail.com>
2026-01-08 12:49:42 -0500

    Problema5 - paquete05

--- ejercicios/Problemas/src/paquete05/Problema5.java
+++ ejercicios/Problemas/src/paquete05/Problema5.java
@@ -10,5 +10,30 @@ package paquete05;
  * @author reroes
  */
 public class Problema5 {
-    
+
+    public static void main(String[] args) {
+        // 
+
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] arregloResultante = llenarMatriz(informacion, informacion2);
+        String cadena = "";
+        for (int i = 0; i < arregloResultante.length; i++) {
+            for (int j = 0; j < arregloResultante[i].length; j++) {
+                cadena = String.format("%s%d\t", cadena, arregloResultante[i][j]);
+            }
+            cadena = String.format("%s\n", cadena);
+        }
+        System.out.println(cadena);
+    }
+
+    public static int[][] llenarMatriz(int a[][], int b[][]) {
+        int resultado[][] = new int[3][3];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                resultado[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        return resultado;
+    }
 }
